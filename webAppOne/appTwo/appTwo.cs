a466ccc395998e0aa8fa52b5945f8730b8726fb4
glenn (surface) <gswonk@hotmail.com>
2014-03-24 16:34:27 -0700

    ok

--- webAppOne/appTwo/appTwo.cs
+++ webAppOne/appTwo/appTwo.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Diagnostics;
+
 namespace appTwo
 {
     class appTwo
     {
         static void Main(string[] args)
         {
+            Debug.Print("<appTwo git='Revision information'/>");
         }
     }
 }
